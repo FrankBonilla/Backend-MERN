1996334cc4f984e83264c2b9c9166992dc60f0fc
Frank Bonilla <frankdeivis_17@hotmail.com>
2020-08-03 21:39:35 +0200

    nuevo cambio del port

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.use(cors());
 //habilitar express.json
 app.use( express.json({extended: true }));
 //puerto de la app
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 //importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
-//arrancar la app    
-app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor esta funcionando en el puerto ${port}`);
+//arrancar la app
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 })
